5385feb63fb870c990a4bed555c6b1ac59de6fa6
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2013-02-26 14:18:42 +0100

    Add documentation.

--- js/gooda/HelpPane.js
+++ js/gooda/HelpPane.js
@@ -58,7 +58,9 @@ require(["dojo/_base/declare",
         identifier: 'name',
         label: 'name',
         items: [
-          {name: 'Welcome'}
+          {name: 'Welcome'},
+          {name: 'Tutorial'},
+          {name: 'Cycle Accounting'}
         ]
       };
       
@@ -81,6 +83,12 @@ require(["dojo/_base/declare",
             case 'Welcome':
               self.showWelcomeScreen();
               break;
+            case 'Tutorial':
+              self.showTutorialScreen();
+              break;
+            case 'Cycle Accounting':
+              self.showCycleAccountingScreen();
+              break;
             default:
               break;
           }
@@ -92,22 +100,46 @@ require(["dojo/_base/declare",
       
       this.parentContainer.addView(this.container);
       
-      this.tutorialPane = new ContentPane({
+      this.welcomePane = new ContentPane({
         title: 'Welcome',
-        content: '<div id="tutorial">\
+        content: '<div class="help" id="tutorial">\
                   <p>Modern superscalar, out-of-order microprocessors dominate large scale server computing. Monitoring their activity, during program execution, has become complicated due to the complexity of the microarchitectures and their IO interactions. Recent processors have thousands of performance monitoring events. These are required to actually provide coverage for all of the complex interactions and performance issues that can occur. Knowing which data to collect and how to interpret the results has become an unreasonable burden for code developers whose tasks are already hard enough.  It becomes the task of the analysis tool developer to bridge this gap.</p>\
                   <img src="images/cycleTree.png" />\
                   <p>To address this issue, a generic decomposition of how a microprocessor is using the consumed cycles allows code developers to quickly understand which of the myriad of microarchitectural complexities they are battling, without requiring a detailed knowledge of the microarchitecture. When this approach is intrinsically integrated into a performance data analysis tool, it enables software developers to take advantage of the microarchitectural methodology that has only been available to experts.\
                   The Generic Optimization Data Analyzer (GOoDA) project integrates this expertise into a profiling tool in order to lower the required expertise of the user and, being designed from the ground up with large-scale object-oriented applications in mind, it will be particularly useful for large HENP codebases.</p></div>'
       });
+
+      this.cycleAccountingPane = new ContentPane({
+        title: 'Cycle Accounting',
+        content: '<div class="help" id="cycleAccounting">\
+                  <iframe src="http://docs.google.com/gview?url=http://gooda.googlecode.com/git/gooda-analyzer/docs/CycleAccountingandPerformanceAnalysis.pdf&embedded=true" style="width:100%; height:700px; display:block; margin-left:auto; margin-right: auto" frameborder="0"></iframe>'
+      });
+
+     this.tutorialPane = new ContentPane({
+        title: 'Tutorial',
+        content: '<div class="help" id="tutorial">\
+                  <iframe src="http://docs.google.com/gview?url=https://github.com/vitillo/talks/blob/master/GoodaTutorial2012.pdf?raw=true&embedded=true" style="width:100%; height:700px; display:block; margin-left:auto; margin-right: auto" frameborder="0"></iframe>'
+      });
       
+      this.container.addChild(this.welcomePane);     
       this.container.addChild(this.tutorialPane);     
+      this.container.addChild(this.cycleAccountingPane);
       this.navigationMenu.addChild(tree);
     },
     
     showWelcomeScreen: function(){
       this.highlight();
+      this.highlightView(this.welcomePane);
+    },
+
+    showTutorialScreen: function(){
+      this.highlight();
       this.highlightView(this.tutorialPane);
+    },
+
+    showCycleAccountingScreen: function(){
+      this.highlight();
+      this.highlightView(this.cycleAccountingPane);
     }
   })
 });
